cba5a980196843551b41f4b4024d563dc960bee3
Longman Xu <longmanxu@live.ca>
2020-02-04 20:49:40 -0500

    Add useless comment
    The most useless comment to ever have been commited.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 int main() {
     std::cout << "Shark of the Sky\n";
     std::cout << 30 * 1000 << "\n";
-    // hwat
-    // AMAZING CHANGE
-    // ANOTHER AMAZING CHANGE
+    // comments are useless
 }
 
